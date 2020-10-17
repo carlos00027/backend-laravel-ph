959daeec22a40f76af4e51f76ec2b31546f4ac80
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2020-10-16 22:35:59 -0500

    migraciones

---
+++ database/migrations/2020_10_17_012225_create_users_roles_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::dropIfExists('users_roles');
+
+        Schema::create('users_roles', function (Blueprint $table) {
+            $table->id();
+
+            $table->bigInteger("user_id")->unsigned();
+            $table->foreign("user_id")
+            ->references("id")
+            ->on("users")
+            ->onDelete("cascade")
+            ->onUpdate("cascade");
+
+            $table->bigInteger("rol_id")->unsigned();
+            $table->foreign("rol_id")
+            ->references("id")
+            ->on("roles")
+            ->onDelete("cascade")
+            ->onUpdate("cascade");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_roles');
+    }
+}
---
+++ database/migrations/2020_10_17_015958_create_medicamentos_table.php
@@ -0,0 +1,64 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicamentos', function (Blueprint $table) {
+            $table->id();
+            $table->string("expediente");
+            $table->string("producto");
+            $table->string("titular");
+            $table->string("registrosanitario")->nullable();
+            $table->string("fecha_expedicion")->nullable();
+            $table->string("fecha_vencimiento")->nullable();
+            $table->string("estadoregistro")->nullable();
+            $table->string("expedientecum")->nullable();
+            $table->string("consecutivocum")->nullable();
+            $table->string("cantidadcum")->nullable();
+            $table->string("descripcioncomercial")->nullable();
+            $table->string("estadocum",555)->nullable();
+            $table->string("fecha_inactivo",1555)->nullable();
+            $table->string("muestramedica",555)->nullable();
+            $table->string("unidad")->nullable();
+            $table->string("atc")->nullable();
+            $table->string("descripcionatc")->nullable();
+            $table->string("viaadministracion")->nullable();
+            $table->string("concentracion")->nullable();
+            $table->string("principioactivo")->nullable();
+            $table->string("unidadmedida")->nullable();
+            $table->string("cantidad",555)->nullable();
+            $table->string("unidadreferencia",555)->nullable();
+            $table->string("formafarmaceutica")->nullable();
+            $table->string("nombrerol")->nullable();
+            $table->string("tiporol")->nullable();
+            $table->string("modalidad")->nullable();
+            $table->string("columnaa")->nullable();
+            $table->string("columnab")->nullable();
+            $table->string("columnac")->nullable();
+            $table->string("columnad")->nullable();
+            $table->string("columnae")->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicamentos');
+    }
+}
