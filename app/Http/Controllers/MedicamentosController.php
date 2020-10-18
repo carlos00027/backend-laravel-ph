f2a8bbb0e252472dc1ff20cc404bffa968d22611
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2020-10-17 20:50:18 -0500

    ajustes

--- app/Http/Controllers/MedicamentosController.php
+++ app/Http/Controllers/MedicamentosController.php
@@ -10,6 +10,11 @@ class MedicamentosController extends Controller
     public function index(Request $request)
     {
         return Medicamentos::select("medicamentos.*")
+        ->where(function($query) use ($request){
+            if(isset($request->valor)){
+                $query->whereRaw("CONCAT(medicamentos.producto,medicamentos.expediente) like ?","%$request->valor%");
+            }
+        })
         ->paginate(100);
     }
 }
