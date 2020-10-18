28c7f9cbcc2d93ba34375cfdc2537bf9798c8b79
Carlos Sanchez <carlos.sanchez.a0@gmail.com>
2020-10-17 19:23:47 -0500

    medicamentos copia

--- app/Http/Controllers/MedicamentosController.php
+++ app/Http/Controllers/MedicamentosController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Medicamentos;
 use Illuminate\Http\Request;
 
 class MedicamentosController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        return Medicamentos::select("medicamentos.*")
+        ->paginate(100);
+    }
 }
